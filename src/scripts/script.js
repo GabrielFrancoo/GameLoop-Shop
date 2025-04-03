5ee0111712b572473963c1c93ecca292ed22ec98
fsluz <67333597+Bytters@users.noreply.github.com>
2025-04-03 12:07:19 -0300

    Add files via upload

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -14,3 +14,23 @@ function abrirModal(url1){
     });
     
 }
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  const x = document.getElementsByClassName("sliderImg");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+ 
+  slideIndex++;
+  
+  if (slideIndex > x.length) {
+    slideIndex = 1
+}
+
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Troca a imagem a cada 2s
+}
